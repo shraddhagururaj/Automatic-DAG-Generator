5645e788603cbe84a86cb57f685e03bef72fd0f4
shraddhagururaj <45054328+shraddhagururaj@users.noreply.github.com>
2020-05-07 18:40:54 -0700

    Update task0_wrapper.py

--- Jupiter_testing/task0_wrapper.py
+++ Jupiter_testing/task0_wrapper.py
@@ -1,3 +1,6 @@
+from ctypes import cdll
+from ctypes import c_char_p
+import ctypes 
 import os
 import time
 import shutil
@@ -5,14 +8,24 @@ import math
 import random
 import subprocess
 
-def task():
+def task(filename,pathin,pathout):
      filename= "task0.c"
-     subprocess.call(["gcc",filename])
-     subprocess.call("./a.out")
-        
+     subprocess.call(["gcc","-o","libtest.so","-shared","-fPIC","task0.c"])
+    
+
+
+     lib = cdll.LoadLibrary("./libtest.so")
+     string_buffers = [ctypes.create_string_buffer(8) for i in range(4)]
+     pointers = (ctypes.c_char_p*4)(*map(ctypes.addressof, string_buffers))
+     lib.main(pointers)
+     results = [s.value for s in string_buffers]
+     print (results)
 
 def main():
-    task()
+	filelist = '1botnet.ipsum'
+	outpath = os.path.join(os.path.dirname(__file__), 'sample_input/')
+	outfile = task(filelist, outpath, outpath)
+	return outfile
     
 if __name__ == "__main__":
     main()
